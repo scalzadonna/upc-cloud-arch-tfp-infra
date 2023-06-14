f242ad9ff709f1ae3174630d142b238a6c5b4af5
Santiago Scalzadonna <sscalzadonna@gmail.com>
2023-06-14 14:40:05 +0200

    Add dynamo table name to init-backed output

--- terraform/init-backend/output.tf
+++ terraform/init-backend/output.tf
@@ -1,9 +1,14 @@
-output "kms_key" {
-  description = "The KMS customer master key to encrypt state buckets."
-  value       = module.remote_state.kms_key.key_id
-}
-
 output "state_bucket" {
   description = "The S3 bucket to store the remote state file."
   value       = module.remote_state.state_bucket.bucket
+}
+
+output "dynamodb_table" {
+  description = "The DynamoDB table to manage lock states"
+  module.remote_state.dynamodb_table.table
+}
+
+output "kms_key" {
+  description = "The KMS customer master key to encrypt state buckets."
+  value       = module.remote_state.kms_key.key_id
 }
