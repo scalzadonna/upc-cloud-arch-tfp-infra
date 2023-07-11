c4d3ada218ed5b27f50acfd8e568b12bc70c76c1
Santiago Scalzadonna <sscalzadonna@gmail.com>
2023-07-11 09:54:37 +0200

    Update tf output to include cluster endpoint

--- terraform/eks-cluster/outputs.tf
+++ terraform/eks-cluster/outputs.tf
@@ -10,7 +10,7 @@ output "cluster_name" {
   value       = aws_eks_cluster.eks_cluster.name
 }
 
-# output "cluster_endpoint" {
-#   description = "Endpoint for EKS control plane"
-#   value       = aws_eks_cluster.eks_cluster.endpoint
-# }
+output "cluster_endpoint" {
+  description = "Endpoint for EKS control plane"
+  value       = aws_eks_cluster.eks_cluster.endpoint
+}
